d2ca879802e44fc35f17c2e891f01e60292c8c46
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2018-05-13 10:50:48 -0600

    Initial NET and NG app.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace todo-list
+{
+    [Route("api/[Controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Greetings() {
+            return Ok("Hello from ASP.NET Core Web API.");
+        }
+    }
+}
