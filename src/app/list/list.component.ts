c4509eca9c16676acb6d982b017aa0b90c9caa07
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2018-05-13 12:50:46 -0600

    Added list of to do items, displayed with *ngFor.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../Item';
+import { ITEMS } from '../mock-items';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+   selector: 'app-list',
+   templateUrl: './list.component.html',
+   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+   items = ITEMS;
 
-  ngOnInit() {
-  }
+   selectedItem: Item;
 
+   constructor() { }
+
+   ngOnInit() {
+   }
+   
+   onSelect(item: Item): void {
+      this.selectedItem = item;
+   }
 }
---
+++ src/app/mock-items.ts
@@ -0,0 +1,14 @@
+import { Item } from './Item';
+
+export const ITEMS: Item[] = [
+   { id: 1, name: 'Shower' },
+   { id: 2, name: 'Workout' },
+   { id: 3, name: 'Learn ASP' },
+   { id: 4, name: 'Learn NG' },
+   { id: 5, name: 'Build App' },
+   { id: 6, name: 'Sleep' },
+   { id: 7, name: 'Eat' },
+   { id: 8, name: 'Code' },
+   { id: 9, name: 'Code' },
+   { id: 10, name: 'Code' }
+];
